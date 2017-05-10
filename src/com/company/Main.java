716bf7146e2c6eb24e018d2c8c7dd9d51c45770e
df377 <df377@pvsd.org>
2017-05-10 14:30:27 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
+
+public class Main  {
+
+    public static void main(String[] args) throws IOException{
+
+        File textInput = new File("rawDataU6 (1).txt");
+        Scanner scan = new Scanner(textInput);
+        scan.useDelimiter("\t,");
+
+        String[] lON = new String[1000]; //lON = list of names
+        int maxIndex = -1;
+        ArrayList<String> funky = new ArrayList<String>();
+
+
+        while(scan.hasNext()) {
+            maxIndex++;
+            funky.add(scan.nextLine());
+        }
+        
+
+
+    }
+}
---
+++ src/com/company/Students.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by df377 on 5/10/17.
+ */
+
+public class Students {
+
+
+    public String name;
+    public int secretNumber;
+    public double qu1;
+    public double qu2;
+
+    public Students(String s, int n, double n1, double n2) {
+        name = s;
+        secretNumber = n;
+        qu1 = n1;
+        qu2 = n2;
+    }
+
+}
